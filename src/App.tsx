aba6a9a88258c0f7f8b32392175e148c0ef05686
anzal1 <cofclan145@gmail.com>
2024-04-23 17:46:20 +0530

    fix: sanitize codebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { ReactElement, useState } from 'react'
 
 function App(): ReactElement {
-  const [count, setCount] = useState(0)
-
   return <div className="p-20 border shadow-xl border-gray-50 rounded-xl"></div>
 }
 
