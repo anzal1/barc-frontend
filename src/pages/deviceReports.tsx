3ca701c7d59fd4273c27952cea6e9c22f64ed73c
m3rashid <m3rashid.hussain@gmail.com>
2024-05-16 10:17:55 +0530

    feat(): logs as table

--- src/pages/deviceReports.tsx
+++ src/pages/deviceReports.tsx
@@ -11,63 +11,68 @@ import dayjs from 'dayjs'
 
 export const DeviceReports = () => {
   const navigate = useNavigate()
-  const [reports, setReports] = React.useState<any[]>([])
+  const [reports, setReports] = React.useState<any[] | null>(null)
 
   return (
     <Layout navType={NavType.FILLED}>
-      <div className="h-full w-full p-12 justify-center items-center">
-        <CustomCard
-          header={
-            <div className="flex  justify-between items-center h-full w-full ">
-              <h1>Device Reports</h1>
-              <div className="flex gap-6">
-                <img
-                  src={cancel}
-                  className="w-10 h-10 cursor-pointer"
-                  alt="cancel"
-                  onClick={() => navigate('/')}
-                />
-              </div>
-            </div>
-          }
-        >
-          {reports.length > 0 ? (
-            <div className="flex flex-col gap-4 items-start h-full">
-              <p
-                className="text-blue-500 cursor-pointer"
-                onClick={() => setReports([])}
-              >
-                Back to form
-              </p>
+      {reports ? (
+        <div className="flex flex-col gap-4 items-start h-full">
+          <p
+            className="text-blue-500 cursor-pointer bg-white rounded-lg p-2 hover:bg-blue-500 hover:text-white m-4 -mb-2 px-8 "
+            onClick={() => setReports(null)}
+          >
+            Back to form
+          </p>
 
-              <Table
-                data={reports.reverse()}
-                rootClassName="w-full max-w-[calc(100vw-160px)]"
-                columns={[
-                  { key: 'logID', title: 'Log ID' },
-                  {
-                    key: 'entryDate',
-                    title: 'Entry Date',
-                    render: ({ entryDate }) =>
-                      dayjs(entryDate).format('DD-MM-YYYY HH:mm:ss A')
-                  },
-                  { key: 'user_name', title: 'User Name' },
-                  { key: 'activity', title: 'Activity' },
-                  {
-                    key: 'log_Discription',
-                    title: 'Log Description',
-                    render: ({ log_Discription }) => (
-                      <p className="w-full break-words">{log_Discription}</p>
-                    )
-                  }
-                ]}
-              />
-            </div>
-          ) : (
+          <Table
+            title="Log Reports"
+            data={reports}
+            rootClassName="w-full max-w-[calc(100vw-48px)] m-[12px] p-2 rounded-lg bg-white shadow-md"
+            columns={[
+              {
+                key: 'logID',
+                title: 'Sl. No.',
+                render: (_, __, index) => index + 1
+              },
+              {
+                key: 'log_Discription',
+                title: 'Description',
+                className: 'text-left',
+                render: ({ log_Discription }) => (
+                  <p className="w-full break-words">{log_Discription}</p>
+                )
+              },
+              { key: 'user_name', title: 'By Whom' },
+              { key: 'activity', title: 'Activity' },
+              {
+                key: 'entryDate',
+                title: 'Activity Date',
+                render: ({ entryDate }) => dayjs(entryDate).format('DD/MM/YYYY')
+              }
+            ]}
+          />
+        </div>
+      ) : (
+        <div className="h-full w-full p-12 justify-center items-center">
+          <CustomCard
+            header={
+              <div className="flex  justify-between items-center h-full w-full ">
+                <h1>Device Reports</h1>
+                <div className="flex gap-6">
+                  <img
+                    src={cancel}
+                    className="w-10 h-10 cursor-pointer"
+                    alt="cancel"
+                    onClick={() => navigate('/')}
+                  />
+                </div>
+              </div>
+            }
+          >
             <DeviceReportsForm setReports={setReports} />
-          )}
-        </CustomCard>
-      </div>
+          </CustomCard>
+        </div>
+      )}
     </Layout>
   )
 }
